d5876dcb0a724509e96dd06d5efa4f75c9440317
Dario Fadda <dfadda@gankien2023.local>
2026-06-02 15:47:00 -0400

    added IO routine

--- fancyroman/io.py
+++ fancyroman/io.py
@@ -1,4 +1,4 @@
-import numpy as np
+from PyQt6.QtWidgets import QDialog, QCheckBox
 
 class WFIimage(object):
     """ WFI image - read with Roman Data Models routines """
@@ -12,5 +12,15 @@ class WFIimage(object):
         self.filename = infile
         with rdm.open(infile) as dm:
             self.image = dm.data.copy()
-            self.dq = dm.dq
-            self.wcs = dm.meta
+            self.dq = dm.dq.copy()
+            self.wcs = dm.meta.wcs
+            
+    def save(self, name='test.asdf'):
+        
+        import roman_datamodels as rdm
+        
+        with rdm.open(self.filename) as f:
+            f.data = self.image
+            f.dq = self.dq
+            f.save(name)
+            
